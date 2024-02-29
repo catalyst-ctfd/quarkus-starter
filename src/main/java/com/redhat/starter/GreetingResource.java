6f060e91512681ff5f3f108450f9d03b4fd3c7e9
nmalvankar <nmalvankar@gmail.com>
2024-02-29 12:24:25 -0500

    Update GreetingResource.java

--- src/main/java/com/redhat/starter/GreetingResource.java
+++ src/main/java/com/redhat/starter/GreetingResource.java
@@ -4,13 +4,27 @@ import jakarta.ws.rs.GET;
 import jakarta.ws.rs.Path;
 import jakarta.ws.rs.Produces;
 import jakarta.ws.rs.core.MediaType;
+import javax.ws.rs.core.HttpHeaders;
 
 @Path("/")
 public class GreetingResource {
 
+    @Inject
+    HttpHeaders httpHeaders;
+
     @GET
     @Produces(MediaType.TEXT_PLAIN)
     public String hello() {
-        return "Hello World";
+        String hostname = httpHeaders.getHeaderString("Host");
+        String response = null;
+        String hello = "SEVMTE9fV09STEQ=";
+        String aloha = "QUxPSEFfR1JFRVRJTkc="
+
+        if (hostname.contains("aloha")) 
+            response = new String(Base64.getDecoder().decode(aloha));
+        else
+            response = new String(Base64.getDecoder().decode(hello));
+        
+        return response;
     }
 }
